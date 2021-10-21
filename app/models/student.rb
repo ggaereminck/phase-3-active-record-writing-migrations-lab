1e8f8bf4e4bdfc9a5ab6ad7d9d4e00fbce22cb98
Gerald <ggaereminck@yahoo.com>
2021-10-21 09:37:59 -0400

    Completed Assignment

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,16 @@
+# ActiveRecord::Base.establish_connection(
+#     adapter: "sqlite3",
+#     database: "db/students.sqlite"
+# )
+
+# sql - <<-SQL
+#     CREATE TABLE IF NOT EXISTS artists (
+#         id INTEGER PRIMARY KEY,
+#         name TEXT
+#     )
+# SQL
+
 class Student < ActiveRecord::Base
 end
+
+# ActiveRecord::Base.connection.execute(sql)
